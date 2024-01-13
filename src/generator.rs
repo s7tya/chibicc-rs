228a8967224e4e02106c062ee34ff2d48494ddd8
s7tya <53410646+s7tya@users.noreply.github.com>
2024-01-14 02:55:58 +0900

    WIP: テストをRustに書き換え

--- src/generator.rs
+++ src/generator.rs
@@ -1,35 +1,37 @@
+use std::io::Write;
+
 use crate::parser::{Node, NodeKind};
 
-pub fn gen(node: Box<Node>) {
+pub fn gen<W: Write>(w: &mut W, node: Box<Node>) {
     if matches!(node.kind, NodeKind::Num(_)) {
         if let NodeKind::Num(n) = node.kind {
-            println!("  push {}", n);
+            writeln!(w, "  push {}", n);
             return;
         }
     }
 
-    gen(node.lhs.unwrap());
-    gen(node.rhs.unwrap());
+    gen(w, node.lhs.unwrap());
+    gen(w, node.rhs.unwrap());
 
-    println!("  pop rdi");
-    println!("  pop rax");
+    writeln!(w, "  pop rdi");
+    writeln!(w, "  pop rax");
 
     match node.kind {
         NodeKind::Add => {
-            println!("  add rax, rdi");
+            writeln!(w, "  add rax, rdi");
         }
         NodeKind::Sub => {
-            println!("  sub rax, rdi");
+            writeln!(w, "  sub rax, rdi");
         }
         NodeKind::Mul => {
-            println!("  imul rax, rdi");
+            writeln!(w, "  imul rax, rdi");
         }
         NodeKind::Div => {
-            println!("  cqo");
-            println!("  idiv rdi");
+            writeln!(w, "  cqo");
+            writeln!(w, "  idiv rdi");
         }
         NodeKind::Num(_) => {}
     }
 
-    println!("  push rax");
+    writeln!(w, "  push rax");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{
+    env,
+    io::{stdout, Write},
+};
 
 mod generator;
 mod parser;
@@ -10,15 +13,32 @@ fn main() {
         panic!("引数の個数が間違っています");
     }
 
-    let tokens = tokenizer::tokenize(&args[1]);
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+    run(&mut stdout, &args[1]);
+}
+
+fn run<W: Write>(w: &mut W, p: &String) {
+    let tokens = tokenizer::tokenize(p);
     let tree = parser::Parser::new(&tokens).parse();
 
-    println!(".intel_syntax noprefix");
-    println!(".globl main");
-    println!("main:");
+    writeln!(w, ".intel_syntax noprefix");
+    writeln!(w, ".globl main");
+    writeln!(w, "main:");
 
-    generator::gen(Box::new(tree));
+    generator::gen(w, Box::new(tree));
 
-    println!("  pop rax");
-    println!("  ret");
+    writeln!(w, "  pop rax");
+    writeln!(w, "  ret");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::run;
+
+    #[test]
+    fn test_asm() {
+        let mut buf = Vec::<u8>::new();
+        run(&mut buf, &String::from("20*5+2"));
+    }
 }
