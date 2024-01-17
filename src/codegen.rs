949b12c0827525f79a96e415d91020659c3c2417
s7tya <53410646+s7tya@users.noreply.github.com>
2024-01-17 23:25:49 +0900

    fix codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -8,57 +8,54 @@ use crate::{
 pub fn gen<W: Write>(w: &mut W, node: &Node) {
     if matches!(node.kind, NodeKind::Num(_)) {
         if let NodeKind::Num(n) = node.kind {
-            let _ = writeln!(w, "  push {}", n);
+            let _ = writeln!(w, "  mov ${n}, %rax");
             return;
         }
     }
 
-    gen(w, node.lhs.as_ref().unwrap());
     gen(w, node.rhs.as_ref().unwrap());
-
-    let _ = writeln!(w, "  pop rdi");
-    let _ = writeln!(w, "  pop rax");
+    let _ = writeln!(w, "  push %rax");
+    gen(w, node.lhs.as_ref().unwrap());
+    let _ = writeln!(w, "  pop %rdi");
 
     match node.kind {
         NodeKind::Add => {
-            let _ = writeln!(w, "  add rax, rdi");
+            let _ = writeln!(w, "  add %rdi, %rax");
         }
         NodeKind::Sub => {
-            let _ = writeln!(w, "  sub rax, rdi");
+            let _ = writeln!(w, "  sub %rdi, %rax");
         }
         NodeKind::Multiply => {
-            let _ = writeln!(w, "  imul rax, rdi");
+            let _ = writeln!(w, "  imul %rdi, %rax");
         }
         NodeKind::Div => {
             let _ = writeln!(w, "  cqo");
-            let _ = writeln!(w, "  idiv rdi");
+            let _ = writeln!(w, "  idiv %rdi");
         }
         // NodeKind::Assign => {}
         NodeKind::Num(_) => {}
         NodeKind::Equal | NodeKind::NotEqual | NodeKind::LessThan | NodeKind::LessThanOrEqual => {
-            let _ = writeln!(w, "  cmp rax, rdi");
+            let _ = writeln!(w, "  cmp %rdi, %rax");
 
             match node.kind {
                 NodeKind::Equal => {
-                    let _ = writeln!(w, "  sete al");
+                    let _ = writeln!(w, "  sete %al");
                 }
                 NodeKind::NotEqual => {
-                    let _ = writeln!(w, "  setne al");
+                    let _ = writeln!(w, "  setne %al");
                 }
                 NodeKind::LessThan => {
-                    let _ = writeln!(w, "  setl al");
+                    let _ = writeln!(w, "  setl %al");
                 }
                 NodeKind::LessThanOrEqual => {
-                    let _ = writeln!(w, "  setle al");
+                    let _ = writeln!(w, "  setle %al");
                 }
                 _ => {}
             }
 
-            let _ = writeln!(w, "  movzb rax, al");
+            let _ = writeln!(w, "  movzb %al %rax");
         }
     }
-
-    let _ = writeln!(w, "  push rax");
 }
 
 pub fn write_asm<W: Write>(w: &mut W, input: &str) {
@@ -77,8 +74,7 @@ pub fn write_asm<W: Write>(w: &mut W, input: &str) {
     //
     // Codegen
     //
-    let _ = writeln!(w, ".intel_syntax noprefix");
-    let _ = writeln!(w, ".globl main");
+    let _ = writeln!(w, "  .globl main");
     let _ = writeln!(w, "main:");
 
     for tree in trees {
